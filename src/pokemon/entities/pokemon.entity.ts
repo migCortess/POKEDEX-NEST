b291bc6324ef28fc383c5d3ab3d7f44881bebc97
migCortes <migcortess@gmail.com>
2023-05-18 11:51:14 -0600

    Add pagination and query of All items

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -3,17 +3,17 @@ import { Document } from "mongoose";
 
 @Schema()
 export class Pokemon extends Document {
-    //id: string; Mongo crea un identificador unico
-    @Prop({
-        unique: true,
-        index: true
-    })
-    name: string;
-    @Prop({
-        unique: true,
-        index: true
-    })
-    no: number;
+  //id: string; Mongo crea un identificador unico
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
 }
 
 export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
